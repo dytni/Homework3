153055c75715633c6458b1ac8e09069096a568a8
dytni <141308843+dytni@users.noreply.github.com>
2023-08-15 20:13:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in =  new Scanner(System.in);
+        Random ran = new Random();
+        int max =50;
+        int min =0;
+        int range = max - min + 1;
+        System.out.println("Enter key:");
+        int key = in.nextInt();
+        int []mas= new int[15];
+        for(int i=0;i<15;i++){
+            int a = ran.nextInt(range)+ min;
+            mas[i]=a;
+            System.out.println(mas[i]);
+            if(mas[i]==key){
+                System.out.println("Key was finded");
+            }
+        }
+
+
+
+    }
+}
